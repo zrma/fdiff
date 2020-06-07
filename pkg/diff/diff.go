79def6a880f0e8ef1702e5a94d34a262ebc53527
zrma <bulbitain@gmail.com>
2020-06-07 14:46:02 +0900

    pkg/diff: implement diff traversal

--- pkg/diff/diff.go
+++ pkg/diff/diff.go
@@ -2,8 +2,11 @@ package diff
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path"
+	"path/filepath"
+	"sort"
 	"strings"
 )
 
@@ -14,8 +17,64 @@ func Diff(lhs, rhs string) {
 		return
 	}
 
-	fmt.Println(lhs)
-	fmt.Println(rhs)
+	fmt.Println("diff between ", lhs, "and", rhs)
+	fmt.Println("--------------------------------------------")
+	lhsList := traverse(lhs)
+	rhsList := traverse(rhs)
+
+	diff(lhsList, rhsList)
+}
+
+func diff(lhsList, rhsList []string) {
+	sort.Strings(lhsList)
+	sort.Strings(rhsList)
+
+	var i, j int
+	for i < len(lhsList) && j < len(rhsList) {
+		switch strings.Compare(lhsList[i], rhsList[j]) {
+		case -1:
+			fmt.Println(lhsList[i])
+			i++
+		case 0:
+			i++
+			j++
+			continue
+		case 1:
+			fmt.Println("                    " + rhsList[j])
+			j++
+		}
+	}
+	for i < len(lhsList) {
+		fmt.Println(lhsList[i])
+		i++
+	}
+	for j < len(rhsList) {
+		fmt.Println("                    " + rhsList[j])
+		j++
+	}
+}
+
+func traverse(route string) []string {
+	var res []string
+	err := filepath.Walk(route,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if path != route {
+				res = append(res,
+					strings.TrimPrefix(
+						strings.TrimPrefix(path, route),
+						"/",
+					),
+				)
+			}
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+	return res
 }
 
 func concat(lhs, rhs string) (string, string, error) {
